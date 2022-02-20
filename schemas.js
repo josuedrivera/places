045b981faf7d2af4c9a86083f45f6fec30a828ab
Josue <josue.bills@gmail.com>
2022-02-19 23:48:22 -0800

    upadte index and place view

--- schemas.js
+++ schemas.js
@@ -48,7 +48,7 @@ module.exports.placeSchema = Joi.object({
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
+        // rating: Joi.number().required().min(1).max(5),
         body: Joi.string().required().escapeHTML()
     }).required()
 })
