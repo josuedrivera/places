11a9e5646bd77191b512317cfe062636755676b0
Josue <josue.bills@gmail.com>
2022-03-21 03:18:49 -0700

    update layout and color scheme

--- app.js
+++ app.js
@@ -85,6 +85,7 @@ const scriptSrcUrls = [
     "https://kit.fontawesome.com/",
     "https://cdnjs.cloudflare.com/",
     "https://cdn.jsdelivr.net",
+    "https://kit.fontawesome.com/",
 ];
 const styleSrcUrls = [
     "https://kit-free.fontawesome.com/",
@@ -94,12 +95,14 @@ const styleSrcUrls = [
     "https://fonts.googleapis.com/",
     "https://fonts.gstatic.com",
     "https://use.fontawesome.com/",
+    "https://kit.fontawesome.com/",
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
     "https://a.tiles.mapbox.com/",
     "https://b.tiles.mapbox.com/",
     "https://events.mapbox.com/",
+    "https://kit.fontawesome.com/",
 ];
 const fontSrcUrls = ["https://res.cloudinary.com/dfuxcsi7w/"];
 
